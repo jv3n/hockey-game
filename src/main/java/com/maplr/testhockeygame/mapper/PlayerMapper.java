abd423cfd25470fdd3d701c1354d81e83f40917c
j6455lienv <venet.julien@gmail.com>
2021-07-28 12:50:14 +0200

    Réalisation du test

--- src/main/java/com/maplr/testhockeygame/mapper/PlayerMapper.java
+++ src/main/java/com/maplr/testhockeygame/mapper/PlayerMapper.java
@@ -1,6 +1,6 @@
 package com.maplr.testhockeygame.mapper;
 
-import com.maplr.testhockeygame.bean.PlayerBean;
+import com.maplr.testhockeygame.entity.Player;
 import com.maplr.testhockeygame.dto.PlayerDto;
 import com.maplr.testhockeygame.mapper.config.StandardMapperConfig;
 import org.mapstruct.Mapper;
@@ -8,8 +8,8 @@ import org.mapstruct.Mapper;
 @Mapper(config = StandardMapperConfig.class)
 public interface PlayerMapper {
 
-	PlayerBean toBean(PlayerDto dto);
+	Player toBean(PlayerDto dto);
 
-	PlayerDto toDto(PlayerBean bean);
+	PlayerDto toDto(Player bean);
 
 }
