0b2f917c4920922405cb07248acdf37a274d2528
j6455lienv <venet.julien@gmail.com>
2021-07-29 15:00:50 +0200

    Ajout liquibase et données d'init + mapping entité et sequences custom

--- src/main/java/com/maplr/testhockeygame/mapper/PlayerMapper.java
+++ src/main/java/com/maplr/testhockeygame/mapper/PlayerMapper.java
@@ -1,6 +1,6 @@
 package com.maplr.testhockeygame.mapper;
 
-import com.maplr.testhockeygame.entity.Player;
+import com.maplr.testhockeygame.bean.PlayerBean;
 import com.maplr.testhockeygame.dto.PlayerDto;
 import com.maplr.testhockeygame.mapper.config.StandardMapperConfig;
 import org.mapstruct.Mapper;
@@ -8,8 +8,8 @@ import org.mapstruct.Mapper;
 @Mapper(config = StandardMapperConfig.class)
 public interface PlayerMapper {
 
-	Player toBean(PlayerDto dto);
+	PlayerBean toBean(PlayerDto dto);
 
-	PlayerDto toDto(Player bean);
+	PlayerDto toDto(PlayerBean bean);
 
 }
